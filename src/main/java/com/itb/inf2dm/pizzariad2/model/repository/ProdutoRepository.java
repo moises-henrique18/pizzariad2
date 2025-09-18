288517054a15f48a7e8be868d27c18fceeb26fd5
moises-henrique18 <moiseshenrique.1810@aluno.barueri.br>
2025-09-18 08:55:36 -0300

    Anotações principais de Model Repository concluida

---
+++ src/main/java/com/itb/inf2dm/pizzariad2/model/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.itb.inf2dm.pizzariad2.model.repository;
+
+
+import com.itb.inf2dm.pizzariad2.model.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
